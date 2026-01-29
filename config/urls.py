343a803d9d5357ffa6df44ead1b6b1cbd676a6c1
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-01-29 11:12:19 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # REST API
+    path('api/', include('feed.urls')),
+
+    # GraphQL
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
