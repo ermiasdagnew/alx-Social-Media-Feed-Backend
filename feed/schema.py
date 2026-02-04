aa57f72ee071e52df06e949cd8d9feba8351d746
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-04 16:07:58 +0300

    Update schema.py

--- feed/schema.py
+++ feed/schema.py
@@ -1,7 +1,12 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Post, Comment, Interaction
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from .models import Post, Comment, Interaction
+
+# -------------------------------
+# GraphQL Types
+# -------------------------------
 
 class PostType(DjangoObjectType):
     class Meta:
@@ -18,12 +23,29 @@ class InteractionType(DjangoObjectType):
         model = Interaction
         fields = '__all__'
 
+# -------------------------------
+# Queries
+# -------------------------------
+
 class Query(graphene.ObjectType):
     all_posts = graphene.List(PostType)
+    all_comments = graphene.List(CommentType)
+    all_interactions = graphene.List(InteractionType)
 
     def resolve_all_posts(root, info):
         return Post.objects.all()
 
+    def resolve_all_comments(root, info):
+        return Comment.objects.all()
+
+    def resolve_all_interactions(root, info):
+        return Interaction.objects.all()
+
+# -------------------------------
+# Mutations
+# -------------------------------
+
+# Create Post
 class CreatePost(graphene.Mutation):
     post = graphene.Field(PostType)
 
@@ -32,8 +54,33 @@ class CreatePost(graphene.Mutation):
 
     def mutate(self, info, content):
         user = info.context.user
+        if not user.is_authenticated:
+            raise Exception("Authentication required")
         post = Post.objects.create(author=user, content=content)
         return CreatePost(post=post)
 
+# Login Mutation
+class LoginMutation(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    message = graphene.String()
+
+    def mutate(self, info, username, password):
+        user = authenticate(username=username, password=password)
+        if not user:
+            return LoginMutation(success=False, message="Invalid credentials")
+        return LoginMutation(success=True, message="Login successful")
+
+# Root Mutation
 class Mutation(graphene.ObjectType):
     create_post = CreatePost.Field()
+    login = LoginMutation.Field()
+
+# -------------------------------
+# Schema
+# -------------------------------
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
