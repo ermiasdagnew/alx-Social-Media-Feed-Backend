13105f3da5ac929c91e0ceeca1ca739567298fed
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-07 10:44:32 +0300

    Update urls.py

--- feed/urls.py
+++ feed/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.http import JsonResponse
 from .views import login_view
+from django.http import JsonResponse
 
 def api_root(request):
     return JsonResponse({"message": "API is running"})
 
 urlpatterns = [
-    path('', api_root),                 # http://127.0.0.1:8000/api/
-    path('auth/login/', login_view, name='login'),  # http://127.0.0.1:8000/api/auth/login/
+    path('', api_root),                     # /api/
+    path('auth/login/', login_view, name='login'),  # /api/auth/login/
 ]
