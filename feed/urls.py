d41b957bb8abdb33851844b1283f9bc44fd6c6c6
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-07 21:57:03 +0300

    Add comment and like post endpoints to URLs

--- feed/urls.py
+++ feed/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, login_view
+from .views import PostViewSet, login_view, add_comment, like_post
 from django.http import JsonResponse
 
 # API root: /api/
@@ -16,4 +16,6 @@ urlpatterns = [
     path('', api_root),                     # /api/
     path('auth/login/', login_view, name='login'),  # /api/auth/login/
     path('', include(router.urls)),         # /api/posts/ handled by PostViewSet
+    path('posts/<int:id>/comments/', add_comment, name='add_comment'),  # /api/posts/1/comments/
+    path('posts/<int:id>/like/', like_post, name='like_post'),           # /api/posts/1/like/
 ]
